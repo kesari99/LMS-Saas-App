fff0b6ebae54369bce5f6244526089b8d7a83f12
kesari99 <kesaridevaraya@gmail.com>
2025-05-25 20:57:59 +0530

    implemet navbar

---
+++ components/NavItems.tsx
@@ -0,0 +1,34 @@
+'use client'
+import Link from "next/link";
+import {usePathname} from "next/navigation";
+import {cn} from "@/lib/utils";
+
+const navItems = [
+    {label: 'Home', href: '/'},
+    {label:'Companions', href: '/companions'},
+    {label:'My Journey', href: '/my-journey'},
+]
+
+const NavItems = () => {
+    const pathname = usePathname()
+
+
+    return (
+        <nav className="flex items-center gap-4">
+            {
+                navItems.map(({label, href}) => (
+                    <Link
+                        className={cn(pathname === href && 'text-primary font-semibold')}
+                        key={label}
+                        href={href}
+                    >
+                            {label}
+                    </Link>
+                ))
+            }
+
+
+        </nav>
+    )
+}
+export default NavItems
